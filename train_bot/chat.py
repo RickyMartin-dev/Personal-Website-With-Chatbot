a70e68d2eec3b654c5a0df802a8ef65b6dcbe066
Ricky Martin <rickylenmartin@gmail.com>
2023-04-05 16:53:11 -0400

    new chat gpt clone implementation

--- train_bot/chat.py
+++ train_bot/chat.py
@@ -1,72 +1,25 @@
-import random
-import json
-import torch
-try:
-    from train_bot.model import NeuralNet
-    from train_bot.nltk_utils import bag_of_words, tokenize
-except:
-    from model import NeuralNet
-    from nltk_utils import bag_of_words, tokenize
+import os
+from llama_index import download_loader, LLMPredictor, GPTSimpleVectorIndex, PromptHelper, ServiceContext
+from langchain import OpenAI
+from train_bot.key import secret_key
 
+os.environ['OPENAI_API_KEY'] = secret_key
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DadJokesReader = download_loader("DadJokesReader")
 
-try:
-    with open('train_bot/intents.json', 'r') as f:
-        intents = json.load(f)
-except:
-    with open('intents.json', 'r') as f:
-        intents = json.load(f)
+loader = DadJokesReader()
+documents = loader.load_data()
 
-try:
-    FILE = "train_bot/data.pth"
-    data = torch.load(FILE)
-except:
-    FILE = "data.pth"
-    data = torch.load(FILE)
-
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
-model_state = data["model_state"]
-
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
+dad_index = GPTSimpleVectorIndex.from_documents(documents)
 
 bot_name = "Monday"
 
 def get_response(msg):
-    sentence = tokenize(msg)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
-
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
-
-    tag = tags[predicted.item()]
-
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                return random.choice(intent['responses'])
+    msg = str(msg)
+    #print("Let's chat! (type 'quit' or 'bye' to exit)")
+    if msg.lower() in ["quit", "bye", "goodbye", "stop"]:
+        resp = "Have a great day!"
+    else:
+        resp = dad_index.query(msg)
     
-    return "I do not understand..."
-
-
-if __name__ == "__main__":
-    print("Let's chat! (type 'quit' to exit)")
-    while True:
-     
-        sentence = input("You: ")
-        if sentence == "quit":
-            break
-
-        resp = get_response(sentence)
-        print(resp)
-
+    return str(resp)
