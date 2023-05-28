dd176d4ca8644eacddd538ce3a629709dfa710eb
Ricky Martin <rickylenmartin@gmail.com>
2023-05-28 01:23:58 -0400

    Added a new and improved model

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from email.mime import message
+#from email.mime import message
 from flask import Flask,render_template,request,jsonify
-from train_bot.chat import get_response
+from src.chat import get_response
 
 app = Flask(__name__)
 
 @app.get("/")
-
 def index_get():
-    return render_template('base.html')
-
+    return render_template('index.html')
 
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
-
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
 
-@app.route("/resume")
-def resume():
-    return render_template("resume.html")
+@app.route("/aboutme")
+def aboutme():
+    return render_template("aboutMe.html")
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
